f5e5b66043d5e36624c3c702fd831481d868bbf0
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-04-18 20:43:16 +0200

    login template finished

--- products/forms.py
+++ products/forms.py
@@ -7,10 +7,10 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['category', 'subcategory', 'title', 'description', 'price', 'rating', 'image']
 
-    # Optionally, you can add validation for the image field
+    
     def clean_image(self):
         image = self.cleaned_data.get('image')
         if not image:
             raise forms.ValidationError("Please upload an image.")
-        # You can add additional validation here if needed
+        
         return image
